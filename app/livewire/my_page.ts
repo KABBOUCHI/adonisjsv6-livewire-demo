afcf206e6c29b364051aa5459b488e58cb08da75
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2024-04-17 18:56:55 +0300

    wip

--- app/livewire/my_page.ts
+++ app/livewire/my_page.ts
@@ -1,6 +1,9 @@
-import { Component, title } from 'adonisjs-livewire'
+import { Component, layout, title } from 'adonisjs-livewire'
 
 @title('My Page Title')
+@layout('components/layouts/main', {
+  sidebar: true,
+})
 export default class MyPage extends Component {
   async render() {
     return '<div class="p-4 font-medium text-lg">My Page</div>'
